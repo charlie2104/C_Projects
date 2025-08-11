dffc4834e58b8d55c917c719a9e9e59eb567486a
charlie bishop <charliebishop21@live.co.uk>
2025-08-11 21:18:12 +0100

    Reads file and prints hex values and offsets

--- hex_viewer/main.c
+++ hex_viewer/main.c
@@ -3,11 +3,23 @@
 
 int main(){
 	FILE* fptr;
+	size_t offset = 0;
+	size_t bytesRead;
+	unsigned char data[16];
 	fptr = fopen("test_file.txt", "rb");
 	if(fptr == NULL) {
 		printf("Error opening file\n");
 	} else {
 		printf("File opened\n");
+		while((bytesRead = fread(data, 1, sizeof(data), fptr)) > 0) {
+			printf("%08zx ", offset);
+
+			for(size_t i =0; i < bytesRead; i++) {
+				printf("%02X ", data[i]);
+			}
+			printf("\n");
+		offset += bytesRead;
+		}
 		fclose(fptr);
 		printf("File closed\n");
 	}

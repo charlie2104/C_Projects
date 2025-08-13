cf2af119116fa4d670c287025ec742b008b876ff
charlie bishop <charliebishop21@live.co.uk>
2025-08-13 21:34:05 +0100

    Added in ascii print section and padding

--- hex_viewer/main.c
+++ hex_viewer/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(){
 	FILE* fptr;
@@ -12,13 +13,32 @@ int main(){
 	} else {
 		printf("File opened\n");
 		while((bytesRead = fread(data, 1, sizeof(data), fptr)) > 0) {
+			//Print offset of data in hex with leading 0s
 			printf("%08zx ", offset);
 
+			//print hex values of data 
 			for(size_t i =0; i < bytesRead; i++) {
 				printf("%02X ", data[i]);
 			}
+			
+				
+			//adding padding if a line doesn't have 16 bytes
+			for(int i = 0; i < (16-bytesRead); i++) {
+				printf("   ");
+			}
+
+			//Print ascii values
+			for(size_t i = 0; i < bytesRead; i++) {
+				if(isprint(data[i])) {
+					printf("%c", data[i]);		
+				} else {
+				 	printf(".");
+				}
+			}
+
 			printf("\n");
-		offset += bytesRead;
+		
+			offset += bytesRead;
 		}
 		fclose(fptr);
 		printf("File closed\n");
